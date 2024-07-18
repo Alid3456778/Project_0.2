839f38a8c5402ddcb879cb17558e5341c51dbac2
Alid3456778 <AlidShaikh05@gmail.com>
2024-07-18 19:44:39 +0530

    updating

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 // const express = require('express');
-// const path = require('path');
 // const app = express();
 // const port = 3000;
 
-// // app.set('view engine','ejs');
-// //app.set('views', path.join(__dirname, 'views'));
+// // Set EJS as the view engine
 // app.set('view engine', 'ejs');
 
-// app.get('/',(req,res)=>{
+// // Serve static files from the 'public' directory
+// app.use(express.static('public'));
+
+// // Define a route to render the index.ejs file
+// app.get('/', (req, res) => {
 //     res.render('index');
 // });
 
-// app.listen(port,()=>{
-//     console.log(`server is running on http://localhost:${port}`);
+
+
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
 // });
-// server.js
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -25,12 +31,52 @@ app.set('view engine', 'ejs');
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Parse JSON bodies
+app.use(express.json());
+
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 // Define a route to render the index.ejs file
 app.get('/', (req, res) => {
     res.render('index');
 });
+function readDataFile() {
+    const dataPath = path.join(__dirname, 'data.json');
+    try {
+        if (!fs.existsSync(dataPath)) {
+            fs.writeFileSync(dataPath, '[]', 'utf8');
+        }
+        return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    } catch (error) {
+        console.error('Error reading data file:', error);
+        return [];
+    }
+}
 
+// Define a route to render the admin.ejs file
+app.get('/admin', (req, res) => {
+    const data = readDataFile();
+    res.render('admin', { packages: data });
+});
 
+// Route to add a new package
+app.post('/admin/add', (req, res) => {
+    const data = readDataFile();
+    const newPackage = req.body;
+    data.push(newPackage);
+    fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(data, null, 2));
+    res.redirect('/admin');
+});
+
+// Route to delete a package
+app.post('/admin/delete/:id', (req, res) => {
+    const data = readDataFile();
+    const id = parseInt(req.params.id);
+    data.splice(id, 1);
+    fs.writeFileSync(path.join(__dirname, 'data.json'), JSON.stringify(data, null, 2));
+    res.redirect('/admin');
+});
 
 // Start the server
 app.listen(port, () => {
